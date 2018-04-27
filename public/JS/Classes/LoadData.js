2fbb58ee7cd46f431be59ea4e69cdbe0f9302b5e
LAPTOP-2MJRKRGJ\Milind <milindm24@gmail.com>
2018-04-26 23:10:15 -0500

    FinalDashboard

--- public/JS/Classes/LoadData.js
+++ public/JS/Classes/LoadData.js
@@ -1,4 +1,4 @@
-class ReadFiles {
+class LoadData {
 
     constructor() {
 
--- public/JS/main.js
+++ public/JS/main.js
@@ -7,123 +7,67 @@ var data = fetch('./public/CSV/header.csv')
 
 
 headerCreate = (header) =>{
-    var createHeader = new CreateHeader();
-    createHeader.headerCreate(header);
+    new Dashboard().headerCreate(header);
 }
 
 
 onChooseFile = () =>{
-    var fileChose = new ReadFiles();
-    fileChose.onChooseFile();
+    new Dashboard().onChooseFile();
  }
  
  onAddFiles = () => {
-     var fileChose = new ReadFiles();
-     fileChose.onAddFiles();
+    new Dashboard().onAddFiles();
  }
  
  
  viewData = (sliceMainArray)=>{
-     var viewTable = new ViewTable();
-     viewTable.viewData(sliceMainArray);
+    new Dashboard().viewData(sliceMainArray);
  }
  
  onCheckCountry = (checkid,className)=>{
-     if (document.getElementById(checkid).checked){
- 
-         var countryClass = document.getElementsByClassName(className);
-         for (var i=0; i<countryClass.length;i++){
-             countryClass[i].style.display = "";
-         }
-     }else {
-         console.log(className);
- 
-         var countryClass = document.getElementsByClassName(className);
-         for (var i=0; i<countryClass.length;i++){
-             countryClass[i].style.display = "none";
-         }
-     }
+    new Dashboard().onCheckCountry(checkid,className);
  }
     
  
  onClickFilter = (value,id)=>{
-     console.log(value + " " + id);
-     var checkIndicator = document.getElementById(id).checked;
-     if (checkIndicator){
-         var countryClass = document.getElementsByClassName(value);
-         // document.getElementById(value).style.display = "";
-         for (var i=0; i<countryClass.length;i++){
-             countryClass[i].style.display = "";
-         }
-     }else {
-         var countryClass = document.getElementsByClassName(value);
-         // document.getElementById(value).style.display = "none";        
-         for (var i=0; i<countryClass.length;i++){
-             countryClass[i].style.display = "none";
-         }
-     }
+    new Dashboard().onClickFilter(value,id);
+
  }
  
  
  onFilterColumns = (type="bar")=>{
- 
-     var filteredColumn = new FilterColumn();
-     filteredColumn.onFilterColumns(type);
-   
+    new Dashboard().onFilterColumns(type);
  }
  
  
  expandCountries = () => {
-    var expandCountries = new LeftNavControl();
-    expandCountries.expandCountries();
+    new Dashboard().expandCountries();
 }
 
 
 expandSliders = ()=>{
-    var expandSliders = new LeftNavControl();
-    expandSliders.expandSliders();
+    new Dashboard().expandSliders();
 }
 
 
 expandCharts = ()=>{
-    var expandCharts = new LeftNavControl();
-    expandCharts.expandCharts();
+    new Dashboard().expandCharts();
 }
 
 
 onfilterRows = ()=>{
-    var onfilterRows = new LeftNavControl();
-    onfilterRows.onfilterRows();
+    new Dashboard().onfilterRows();
 }
 
 
 expandStats = ()=>{
-    var expandStats = new LeftNavControl();
-    expandStats.expandStats();
+    new Dashboard().expandStats();
 } 
 
 onViewChart = (chartArray=copyArray,chartHeader=headerArray,chartType="bar")=>{
-    var createChart = new CreateChart();
-    createChart.onViewChart(chartArray,chartHeader,chartType);
+    new Dashboard().onViewChart(chartArray,chartHeader,chartType);
  }
 
-
-
  onShowStats = (value)=>{
-    if (value==="min"){
-        var min = new Min();
-        min.calculate();
-    }
-    else if (value==="max"){
-        var max = new Max();
-        max.calculate();
-    }
-    else if (value==="avg"){
-        var avg = new Average();
-        avg.calculate();
-    }
-    else if (value==="sum"){
-        var sum = new Sum();
-        sum.calculate();
-    }
+    new Dashboard().onShowStats(value);
 }
